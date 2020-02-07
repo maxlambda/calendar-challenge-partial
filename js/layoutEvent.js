f2486bdb0950253b45d6330c9586f1f87a041f1d
Max Lam <57158526+maxlambda@users.noreply.github.com>
2020-02-07 02:58:48 -0500

    Add files via upload

---
+++ js/layoutEvent.js
@@ -0,0 +1,31 @@
+// jshint esversion:6
+
+let layoutEvent = (function() {
+	let maxHeight = 720;
+	let maxWidth = 600;
+	let tree;
+	let eventCont = $('.events-container');
+
+	// make a function that adds an event to the tree
+  //	function addEvent(event, item, position) {
+  //
+  //		item.offset = position++;
+  //		event.children.push(item);
+  //		item.parent = node;
+  //		tree.push(item);
+  //
+  //	}
+
+
+	// make a function that determines whether or not events overlap
+
+	// make a function to iterate over an event list and see if thei
+	//   placement is successful
+
+	// make a function that sets the widths of each event, which also
+	//   tries to find the maximum width and error in distance
+
+	// create a function that initializes the tree startin at 0 to 720
+
+	// return the laid-out event
+})();
